09905769bebbc30b97b45208667780758a0b01da
mnathsnyk <162179737+mnathsnyk@users.noreply.github.com>
2024-12-04 11:19:01 -0500

    deleting nosql

--- routes/likeProductReviews.js
+++ routes/likeProductReviews.js
@@ -8,29 +8,7 @@ const challenges = require('../data/datacache').challenges
 const db = require('../data/mongodb')
 const insecurity = require('../lib/insecurity')
 
-module.exports = function productReviews () {
-  return (req, res, next) => {
-    const id = req.body.id
-    const user = insecurity.authenticatedUsers.from(req)
-    db.reviews.findOne({ _id: id }).then(review => {
-      var likedBy = review.likedBy
-      if (!likedBy.includes(user.data.email)) {
-        db.reviews.update(
-          { _id: id },
-          { $inc: { likesCount: 1 } }
-        ).then(
-          result => {
-            // Artificial wait for timing attack challenge
-            setTimeout(function () {
-              db.reviews.findOne({ _id: id }).then(review => {
-                var likedBy = review.likedBy
-                likedBy.push(user.data.email)
-                var count = 0
-                for (var i = 0; i < likedBy.length; i++) {
-                  if (likedBy[i] === user.data.email) {
-                    count++
-                  }
-                }
+
                 utils.solveIf(challenges.timingAttackChallenge, () => { return count > 2 })
                 db.reviews.update(
                   { _id: id },
